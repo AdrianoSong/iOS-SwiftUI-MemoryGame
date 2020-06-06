46c598a806a434ca85f34edbbfcd77774df0a436
Adriano Song <adrianosong@hotmail.com>
2020-06-06 11:13:21 -0300

    Initial Commit

---
+++ memoria/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memoria
+//
+//  Created by Song on 06/06/20.
+//  Copyright © 2020 Adriano Song. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
